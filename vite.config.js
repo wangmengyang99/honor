230bebac4e410f491833a2ec61931d888a09a0da
wmy <2137633492@qq.com>
2023-10-16 21:40:27 +0800

    vite.config.js add base option

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+/*
+ * @Author: wmy 2137633492@qq.com
+ * @Date: 2023-10-16 21:17:22
+ * @LastEditors: wmy 2137633492@qq.com
+ * @LastEditTime: 2023-10-16 21:37:52
+ * @FilePath: \honor\vite.config.js
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -8,6 +18,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base:'./',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
